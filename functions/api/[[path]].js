30ff608e83f79b279d6f0771e96e41df2448ec2b
aanm31 <aanm31@gmail.com>
2026-06-20 02:18:02 +0300

    ة

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -318,7 +318,7 @@ function validateAccount(body) {
 
 async function hashPassword(password, salt = randomToken(16)) {
   const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(password), "PBKDF2", false, ["deriveBits"]);
-  const bits = await crypto.subtle.deriveBits({ name: "PBKDF2", hash: "SHA-256", salt: fromBase64Url(salt), iterations: 120000 }, key, 256);
+  const bits = await crypto.subtle.deriveBits({ name: "PBKDF2", hash: "SHA-256", salt: fromBase64Url(salt), iterations: 20000 }, key, 256);
   return { salt, hash: toBase64Url(new Uint8Array(bits)) };
 }
 async function verifyPassword(password, salt, expected) { const value = await hashPassword(password, salt); return constantTime(value.hash, expected); }
